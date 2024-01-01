7e461f15aa6179507b1fbb463fa390c8f151b84e
Bhupendra <bhupendrabc71@gmail.com>
2024-01-02 00:49:47 +1030

    Added pause, restart buttons

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+let animationId; // Declare animationId outside the event listener
+
 document.addEventListener("DOMContentLoaded", function () {
     const ball = document.getElementById("ball");
     const box = document.querySelector(".box");
     const hitSound = document.getElementById("hitSound");
+    const pauseResumeButton = document.getElementById("pauseResumeButton");
+    const restartButton = document.getElementById("restartButton");
 
     let posX = 6;
     let posY = 0;
@@ -32,8 +36,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function animate() {
         updateBallPosition();
-        requestAnimationFrame(animate);
+        animationId = requestAnimationFrame(animate);
     }
 
+    function toggleAnimation() {
+        if (animationId) {
+            cancelAnimationFrame(animationId);
+            animationId = undefined;
+            pauseResumeButton.innerText = "Resume";
+        } else {
+            animate();
+            pauseResumeButton.innerText = "Pause";
+        }
+    }
+
+    function restart() {
+        posX = 6;
+        posY = 0;
+        speedX = 7;
+        speedY = 5;
+        ball.style.transform = `translate(${posX}px, ${posY}px)`;
+        if (animationId === undefined) {
+            animate();
+        }
+        pauseResumeButton.innerText = "Pause";
+    }
+
+    pauseResumeButton.addEventListener("click", toggleAnimation);
+    restartButton.addEventListener("click", restart);
+
+    // Initial animation
     animate();
 });
