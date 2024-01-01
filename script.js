6d164d2670639c0b42b0e1b61ecc354334e91692
Bhupendra <bhupendrabc71@gmail.com>
2024-01-01 23:38:53 +1030

    Initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const box = document.querySelector(".box");
+    const audio = new Audio("hit_sound.mp3"); // Replace "hit_sound.mp3" with the path to your audio file
+
+    // Create a new element for the ball trail
+    const trail = document.createElement("div");
+    trail.classList.add("trail");
+    box.appendChild(trail);
+
+    const ball = document.getElementById("ball");
+
+    let posX = 5;
+    let posY = 0;
+    let speedX = 5;
+    let speedY = 2;
+
+    function updateBallPosition() {
+        posX += speedX;
+        posY += speedY;
+
+        if (posX < 0 || posX > box.clientWidth - ball.clientWidth) {
+            speedX = -speedX;
+            playHitSound(); // Play sound on wall hit
+        }
+
+        if (posY < 0 || posY > box.clientHeight - ball.clientHeight) {
+            speedY = -speedY;
+            playHitSound(); // Play sound on wall hit
+        }
+
+        // Update the ball's position
+        ball.style.transform = `translate(${posX}px, ${posY}px)`;
+
+        // Update the trail's position
+        trail.style.left = `${posX}px`;
+        trail.style.top = `${posY}px`;
+
+        // Create a new trail element
+        const newTrail = document.createElement("div");
+        newTrail.classList.add("trail");
+        newTrail.style.left = `${posX}px`;
+        newTrail.style.top = `${posY}px`;
+
+        // Append the new trail element to the box
+        box.appendChild(newTrail);
+
+        // Remove the oldest trail element
+        if (box.children.length > 50) {
+            box.removeChild(box.children[0]);
+        }
+    }
+
+    function playHitSound() {
+        audio.currentTime = 0; // Reset the audio to the beginning
+        audio.play();
+    }
+
+    function animate() {
+        updateBallPosition();
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+});
